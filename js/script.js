d728064a7264d03d3a3c552ac0e6690ba8fb041b
Paolo Cannistraro <wasdgamer91@gmail.com>
2022-03-31 12:37:10 +0200

    some corrections

--- js/script.js
+++ js/script.js
@@ -26,42 +26,69 @@ let prevButton = document.getElementById('prevButton');
 let nextButton = document.getElementById('nextButton')
 let itemActive = document.getElementById('active');
 let thumbContainer = document.getElementById('thumbContainer');
-
-const thumbnails = document.getElementsByClassName('item');
-
-const itemsContainer = document.getElementById('items-container')
+let items = document.getElementsByClassName('items');
+const itemsContainer = document.getElementById('items-container');
+const thumbnails = document.getElementsByClassName('thumbnail');
 
 
 console.log(itemActive)
 console.log(nextButton)
 
 
+
 for (let k = 0; k < imagesArray.length; k++) {
-    thumbContainer.innerHTML += `<div class= "thumbnail">
-    <img class="w-100 d-block" src = "${imagesArray[k]}">
-    </div>`
+
 }
-
-for (let k = 1; k < imagesArray.length; k++) {
-    itemsContainer.innerHTML += `<div class="items d-none">
-    <img class="w-100" src = "${imagesArray[k]}">
-    </div>`
-}
-let items = document.getElementsByClassName('items')
-
-
-
-console.log(items)
-
-console.log(imagesArray)
 
 
 let currentIndex = 0;
+
+for (let i = 0; i < imagesArray.length; i++) {
+
+    let classActive = ""
+    if (i === currentIndex) {
+        classActive = "active"
+    }
+    itemsContainer.innerHTML += `<div class="items ${classActive} ">
+                                    <img src="${imagesArray[i]}">
+                                    <div class="container">
+                                    <div class="row justify-content-center align-items-center">
+                                    <div class="col-12">
+                                        <div class="title">
+                                            <h2 class="h2">${titlesArray[i]}</h2>
+                                            <p class="text">${text[i]}.</p>
+                                    </div>
+                                       
+                                    </div>
+                          
+                                    </div>
+                                        
+                                    </div>`
+
+
+    thumbContainer.innerHTML += `<div class= "thumbnail ${classActive}">
+                                    <img class="w-100" src = "${imagesArray[i]}">
+                                    </div>`
+
+}
+
+//let firstItem = document.querySelector(".items").classList.add("active");
+
+
+console.log(titlesArray)
+console.log(items)
+console.log(imagesArray)
+
+
 nextButton.addEventListener("click", function () {
     if (currentIndex < 4) {
         items[currentIndex].classList.add("d-none");
-        items[currentIndex].classList.remove("active"); 
-        currentIndex += 1;
+        items[currentIndex].classList.remove("active");
+        thumbnails[currentIndex].classList.remove("active");
+      
+        currentIndex ++;
+        
+        thumbnails[currentIndex].classList.add("active");
         items[currentIndex].classList.remove("d-none");
         items[currentIndex].classList.add("active");
     }
@@ -71,9 +98,11 @@ nextButton.addEventListener("click", function () {
 prevButton.addEventListener('click', function () {
 
     if (currentIndex > 0) {
+        thumbnails[currentIndex].classList.remove("active");
         items[currentIndex].classList.add("d-none");
-        items[currentIndex].classList.remove("active"); 
-        currentIndex -= 1;
+        items[currentIndex].classList.remove("active");
+        currentIndex --;
+        thumbnails[currentIndex].classList.add("active");
         items[currentIndex].classList.remove("d-none");
         items[currentIndex].classList.add("active");
     }
